5a2da1cbb250ad6516c5e62e0918d5b23a6092af
kiran <kiran@neostack.com>
2019-10-27 06:42:54 -0400

    useParams

--- src/PostPage.js
+++ src/PostPage.js
@@ -2,13 +2,10 @@ import React from "react"
 import Post from "./Post"
 import Page from "./Page"
 import { POST_QUERY } from "./queries"
+import { useParams } from "react-router-dom"
 
 export default function PostPage(props) {
-  const {
-    match: {
-      params: { id }
-    }
-  } = props
+  const { id } = useParams()
   const postId = parseInt(id)
   return (
     <Page query={POST_QUERY} variables={{ id: postId }}>
