df9b8d3917d8e413c620eff7c2f654477b106671
kiran <kiran@neostack.com>
2019-11-07 05:12:15 +0530

    markdown

--- src/components/TopicEdit.js
+++ src/components/TopicEdit.js
@@ -1,5 +1,24 @@
 import React from "react"
+import { useQuery } from "@apollo/react-hooks"
+import { TOPIC_MARKDOWN_QUERY } from "../data/queries"
+import { Input, Form, FormGroup, Row, Col } from "reactstrap"
 
-export default function TopicEdit() {
-  return <div>edit</div>
+export default function TopicEdit(props) {
+  const { data } = useQuery(TOPIC_MARKDOWN_QUERY, {
+    variables: { id: props.id }
+  })
+  return (
+    <Form>
+      <FormGroup>
+        <Row>
+          <textarea
+            class="form-control"
+            rows="20"
+            value={data.topic.markdown}
+            style={{ width: "600px" }}
+          ></textarea>
+        </Row>
+      </FormGroup>
+    </Form>
+  )
 }
