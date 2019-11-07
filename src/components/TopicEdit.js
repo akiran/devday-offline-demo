8de44eeff4e33a2baec528a35c1fc1a6542121e4
kiran <kiran@neostack.com>
2019-11-07 05:45:02 +0530

    update topic

--- src/components/TopicEdit.js
+++ src/components/TopicEdit.js
@@ -1,12 +1,25 @@
 import React from "react"
-import { useQuery } from "@apollo/react-hooks"
+import { useQuery, useMutation } from "@apollo/react-hooks"
 import { TOPIC_MARKDOWN_QUERY } from "../data/queries"
-import { Input, Form, FormGroup, Row, Col } from "reactstrap"
+import { Form, FormGroup, Row } from "reactstrap"
+import gql from "graphql-tag"
+
+const updateTopicMutation = gql`
+  mutation updateTopicMutation($id: String!, $markdown: String) {
+    updateTopic(id: $id, markdown: $markdown) {
+      id
+      html
+      markdown
+    }
+  }
+`
 
 export default function TopicEdit(props) {
   const { data } = useQuery(TOPIC_MARKDOWN_QUERY, {
     variables: { id: props.id }
   })
+  const [updateTopic, res] = useMutation(updateTopicMutation)
+
   return (
     <Form>
       <FormGroup>
@@ -15,6 +28,11 @@ export default function TopicEdit(props) {
             class="form-control"
             rows="20"
             value={data.topic.markdown}
+            onChange={e =>
+              updateTopic({
+                variables: { id: props.id, markdown: e.target.value }
+              })
+            }
             style={{ width: "600px" }}
           ></textarea>
         </Row>
